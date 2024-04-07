9196dbbeb1a4de0231735dd4b94d6e1b2e0a3df2
Teinturier <henri.teinturier@gmail.com>
2024-04-07 14:32:01 +0200

    clean html from scrapping with cheerios

--- nextjs.dev.ts
+++ nextjs.dev.ts
@@ -1,5 +1,6 @@
 import { chromium } from "playwright";
 import fs from "fs/promises";
+import * as cheerio from "cheerio";
 
 const run = async () => {
   const browser = await chromium.launch();
@@ -22,33 +23,78 @@ const run = async () => {
     })
   );
 
+  const wrapTextWithHtmlTags = (html: string) => {
+    const $ = cheerio.load(html);
+
+    $("h1").each((i, el) => {
+      const text = $(el).text();
+      $(el).text(`[titre principal]${text}[/titre principal]`);
+    });
+
+    $("h2").each((i, el) => {
+      const text = $(el).text();
+      $(el).text(`[sous-titre]${text}[/sous-titre]`);
+    });
+    $("p").each((i, el) => {
+      const text = $(el).text();
+      $(el).text(`[paragraphe]${text}[/paragraphe]`);
+    });
+    $("a").each((i, el) => {
+      const text = $(el).text();
+      const href = $(el).attr("href");
+      $(el).replaceWith(`[lien href="${href}"]${text}[/lien]`);
+    });
+    $("table").each((i, el) => {
+      const text = $(el).text();
+      $(el).text(`[tableau]${text}[/tableau]`);
+    });
+    $("code").each((i, el) => {
+      const text = $(el).text();
+      $(el).text(`[code]${text}[/code]`);
+    });
+    $("img").each((i, el) => {
+      const altText = $(el).attr("alt") || "Image sans texte alternatif";
+      $(el).replaceWith(`[image]${altText}[/image]`);
+    });
+
+    const text = $.text();
+
+    return text;
+  };
+
+  let totalSave = 0;
   for (const url of urls) {
-    console.log("👀 visiting url:", url);
-    await page.goto(`https://nextjs.org/${url}`);
-
-    //get the content of the div with .prose.prose-vercel
-    const content = await page.$eval(
-      "div.prose.prose-vercel",
-      (el) => el.textContent
-    );
-
+    totalSave++;
     if (!url) {
       continue;
     }
 
-    if (!content) {
+    console.log("👀 visiting url:", url);
+    await page.goto(`https://nextjs.org/${url}`);
+
+    const html = await page.$eval(
+      "div.prose.prose-vercel",
+      (el) => el.innerHTML
+    );
+
+    if (!html) {
       continue;
     }
 
-    // const completeUrl = `https://nextjs.org${url}`;
-    // console.log("completeUrl", completeUrl);
+    const text = wrapTextWithHtmlTags(html);
 
-    const encodedUrlForFileName = url.replace(/\//g, "_");
-    const filePath = `./data/nextjs/${encodedUrlForFileName}.txt`;
-    console.log("🚀 Save " + filePath);
-    fs.writeFile(filePath, content);
+    const cleanedText = text
+      .split("\n")
+      .filter((line) => line.trim().length > 0)
+      .join("\n");
+    const fileName =
+      url.replace(/^https?:\/\//, "").replace(/[^a-z0-9]/gi, "_") + ".txt";
+    const filePath = `./data/nextjs/${fileName}`;
+    console.log("🚀 Save " + filePath + " " + totalSave + "/" + urls.length);
+
+    fs.writeFile(filePath, cleanedText, "utf8");
   }
-
+  console.log("end");
   await browser.close();
 };
 
